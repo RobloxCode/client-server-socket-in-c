47123204228799dce0d7bb474fdec5dd44867235
Miguel Loranca <mloranca703@gmail.com>
2026-08-22 20:10:57 -0600

    refactored the server

--- client.c
+++ client.c
@@ -1,40 +1,47 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 8080
+#define BUFCAP 1024
+#define PORT   8080
 
-int create_socket();
-void connect_to_server(int *client_fd);
+int create_socket(void);
+void connect_to_server(int client_fd);
 
 int main(void) {
-    char client_msg[1024] = "hello from client";
-    char server_buf[1024] = {0};
+    char client_buf[] = "hello from client";
+    char server_buf[BUFCAP] = {0};
 
     int client_fd = create_socket();
-    int server_fd = create_socket();
 
-    connect_to_server(&client_fd);
+    connect_to_server(client_fd);
+
+    printf("Connected to server!\n");
 
     while (1) {
-        if (send(client_fd, client_msg, sizeof client_msg - 1, 0) == -1) {
-            fprintf(stderr, "Error sending message to client\n");
+        if (send(client_fd, client_buf, strlen(client_buf), 0) == -1) {
+            perror("send");
             exit(EXIT_FAILURE);
         }
 
-        ssize_t bytes_recieved =
+        printf("Sent to server: %s\n", client_buf);
+
+        ssize_t bytes_received =
             recv(client_fd, server_buf, sizeof server_buf - 1, 0);
 
-        if (bytes_recieved > 0) {
-            printf("Recieved from client: %s\n", server_buf);
-        } else if (bytes_recieved == 0) {
-            printf("End of connection\n");
+        if (bytes_received > 0) {
+            server_buf[bytes_received] = '\0';
+
+            printf("Received from server: %s\n", server_buf);
+        } else if (bytes_received == 0) {
+            printf("Server disconnected\n");
             break;
         } else {
-            fprintf(stderr, "Error connecting to client\n");
-            exit(EXIT_FAILURE);
+            perror("recv");
+            break;
         }
     }
 
@@ -43,38 +50,25 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-int create_socket() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+int create_socket(void) {
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (server_fd == -1) {
-        fprintf(stderr, "error creating socket\n");
+    if (client_fd == -1) {
+        perror("socket");
         exit(EXIT_FAILURE);
     }
 
-    return server_fd;
+    return client_fd;
 }
 
-void bind_socket(int *server_fd) {
-    struct sockaddr_in addr = {0};
-
-    addr.sin_family = AF_INET;
-    addr.sin_port = htonl(PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-    if (bind(*server_fd, (struct sockaddr *)&addr, sizeof addr) == -1) {
-        fprintf(stderr, "error binding socket\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void connect_to_server(int *client_fd) {
+void connect_to_server(int client_fd) {
     struct sockaddr_in server_addr = {.sin_family = AF_INET,
                                       .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
-                                      .sin_port = htons(8080)};
+                                      .sin_port = htons(PORT)};
 
-    if (connect(*client_fd, (struct sockaddr *)&server_addr, sizeof server_addr)
+    if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof server_addr)
         == -1) {
-        fprintf(stderr, "Error sending message to client\n");
+        perror("connect");
         exit(EXIT_FAILURE);
     }
 }
--- server.c
+++ server.c
@@ -1,55 +1,67 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 8080
+#define BUFCAP 1024
+#define PORT   8080
 
-int create_socket();
-void bind_socket(int *server_fd);
-void listen_socket(int *server_fd, int n);
+int create_socket(void);
+void bind_socket(int server_fd);
+void listen_socket(int server_fd, int n);
 
 int main(void) {
-    char client_buf[1024] = {0};
-    char server_buf[1024] = "hello from server";
+    char client_buf[BUFCAP] = {0};
+    char server_buf[BUFCAP] = "hello from server";
+
     int server_fd = create_socket();
 
-    bind_socket(&server_fd);
+    bind_socket(server_fd);
+    listen_socket(server_fd, 10);
 
-    listen_socket(&server_fd, 10);
+    printf("waiting for client...\n");
 
     int client_fd = accept(server_fd, NULL, NULL);
 
-    while (1) {
-        recv(client_fd, client_buf, sizeof client_buf - 1, 0);
-        printf("Recieved from client: %s\n", client_buf);
+    if (client_fd == -1) {
+        fprintf(stderr, "Error acceting to client\n");
+        exit(EXIT_FAILURE);
+    }
 
+    printf("client connected\n");
+
+    while (1) {
         ssize_t bytes_recieved =
-            recv(client_fd, server_buf, sizeof server_buf - 1, 0);
+            recv(client_fd, client_buf, sizeof client_buf - 1, 0);
 
         if (bytes_recieved > 0) {
-            printf("Recieved from client: %s\n", server_buf);
+            client_buf[bytes_recieved] = '\0';
+            printf("Recieved from client: %s\n", client_buf);
         } else if (bytes_recieved == 0) {
             printf("End of connection\n");
             break;
         } else {
-            fprintf(stderr, "Error connecting to client\n");
+            fprintf(stderr, "Client disconnected\n");
             exit(EXIT_FAILURE);
         }
 
-        if (send(client_fd, server_buf, sizeof server_buf - 1, 0) == -1) {
+        if (send(client_fd, server_buf, strlen(server_buf), 0) == -1) {
             fprintf(stderr, "Error sending message to client\n");
             exit(EXIT_FAILURE);
         }
+
+        printf("Sent to client: %s\n", server_buf);
     }
 
     close(server_fd);
+    close(client_fd);
 
     return EXIT_SUCCESS;
 }
 
-int create_socket() {
+int create_socket(void) {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (server_fd == -1) {
@@ -60,21 +72,21 @@ int create_socket() {
     return server_fd;
 }
 
-void bind_socket(int *server_fd) {
+void bind_socket(int server_fd) {
     struct sockaddr_in addr = {0};
 
     addr.sin_family = AF_INET;
     addr.sin_port = htonl(PORT);
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    if (bind(*server_fd, (struct sockaddr *)&addr, sizeof addr) == -1) {
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof addr) == -1) {
         fprintf(stderr, "error binding socket\n");
         exit(EXIT_FAILURE);
     }
 }
 
-void listen_socket(int *server_fd, int n) {
-    if (listen(*server_fd, n) == -1) {
+void listen_socket(int server_fd, int n) {
+    if (listen(server_fd, n) == -1) {
         fprintf(stderr, "error binding socket\n");
         exit(EXIT_FAILURE);
     }
