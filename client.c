98b78eef43948fa30769203d31167e6c69ba0790
Miguel Loranca <mloranca703@gmail.com>
2026-08-22 20:20:10 -0600

    refactored client and server

--- client.c
+++ client.c
@@ -10,43 +10,10 @@
 
 int create_socket(void);
 void connect_to_server(int client_fd);
+void start_client(void);
 
 int main(void) {
-    char client_buf[] = "hello from client";
-    char server_buf[BUFCAP] = {0};
-
-    int client_fd = create_socket();
-
-    connect_to_server(client_fd);
-
-    printf("Connected to server!\n");
-
-    while (1) {
-        if (send(client_fd, client_buf, strlen(client_buf), 0) == -1) {
-            perror("send");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Sent to server: %s\n", client_buf);
-
-        ssize_t bytes_received =
-            recv(client_fd, server_buf, sizeof server_buf - 1, 0);
-
-        if (bytes_received > 0) {
-            server_buf[bytes_received] = '\0';
-
-            printf("Received from server: %s\n", server_buf);
-        } else if (bytes_received == 0) {
-            printf("Server disconnected\n");
-            break;
-        } else {
-            perror("recv");
-            break;
-        }
-    }
-
-    close(client_fd);
-
+    start_client();
     return EXIT_SUCCESS;
 }
 
@@ -71,4 +38,40 @@ void connect_to_server(int client_fd) {
         perror("connect");
         exit(EXIT_FAILURE);
     }
+}
+
+void start_client(void) {
+    char client_buf[] = "hello from client";
+    char server_buf[BUFCAP] = {0};
+
+    int client_fd = create_socket();
+
+    connect_to_server(client_fd);
+
+    printf("Connected to server!\n");
+
+    while (1) {
+        if (send(client_fd, client_buf, strlen(client_buf), 0) == -1) {
+            perror("send");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Sent to server: %s\n", client_buf);
+
+        ssize_t bytes_received =
+            recv(client_fd, server_buf, sizeof server_buf - 1, 0);
+
+        if (bytes_received > 0) {
+            server_buf[bytes_received] = '\0';
+            printf("Received from server: %s\n", server_buf);
+        } else if (bytes_received == 0) {
+            printf("Server disconnected\n");
+            break;
+        } else {
+            perror("recv");
+            break;
+        }
+    }
+
+    close(client_fd);
 }
--- server.c
+++ server.c
@@ -11,53 +11,11 @@
 int create_socket(void);
 void bind_socket(int server_fd);
 void listen_socket(int server_fd, int n);
+int accept_client(int server_fd);
+void start_server(void);
 
 int main(void) {
-    char client_buf[BUFCAP] = {0};
-    char server_buf[BUFCAP] = "hello from server";
-
-    int server_fd = create_socket();
-
-    bind_socket(server_fd);
-    listen_socket(server_fd, 10);
-
-    printf("waiting for client...\n");
-
-    int client_fd = accept(server_fd, NULL, NULL);
-
-    if (client_fd == -1) {
-        fprintf(stderr, "Error acceting to client\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("client connected\n");
-
-    while (1) {
-        ssize_t bytes_recieved =
-            recv(client_fd, client_buf, sizeof client_buf - 1, 0);
-
-        if (bytes_recieved > 0) {
-            client_buf[bytes_recieved] = '\0';
-            printf("Recieved from client: %s\n", client_buf);
-        } else if (bytes_recieved == 0) {
-            printf("End of connection\n");
-            break;
-        } else {
-            fprintf(stderr, "Client disconnected\n");
-            exit(EXIT_FAILURE);
-        }
-
-        if (send(client_fd, server_buf, strlen(server_buf), 0) == -1) {
-            fprintf(stderr, "Error sending message to client\n");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Sent to client: %s\n", server_buf);
-    }
-
-    close(server_fd);
-    close(client_fd);
-
+    start_server();
     return EXIT_SUCCESS;
 }
 
@@ -90,4 +48,57 @@ void listen_socket(int server_fd, int n) {
         fprintf(stderr, "error binding socket\n");
         exit(EXIT_FAILURE);
     }
+}
+
+int accept_client(int server_fd) {
+    int client_fd = accept(server_fd, NULL, NULL);
+
+    if (client_fd == -1) {
+        fprintf(stderr, "Error acceting to client\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return client_fd;
+}
+
+void start_server(void) {
+    char client_buf[BUFCAP] = {0};
+    char server_buf[BUFCAP] = "hello from server";
+
+    int server_fd = create_socket();
+
+    bind_socket(server_fd);
+    listen_socket(server_fd, 10);
+
+    printf("waiting for client...\n");
+
+    int client_fd = accept_client(server_fd);
+
+    printf("client connected\n");
+
+    while (1) {
+        ssize_t bytes_recieved =
+            recv(client_fd, client_buf, sizeof client_buf - 1, 0);
+
+        if (bytes_recieved > 0) {
+            client_buf[bytes_recieved] = '\0';
+            printf("Recieved from client: %s\n", client_buf);
+        } else if (bytes_recieved == 0) {
+            printf("End of connection\n");
+            break;
+        } else {
+            fprintf(stderr, "Client disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (send(client_fd, server_buf, strlen(server_buf), 0) == -1) {
+            fprintf(stderr, "Error sending message to client\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Sent to client: %s\n", server_buf);
+    }
+
+    close(server_fd);
+    close(client_fd);
 }
